dc60a1052a384d4c5801ee147ff5c4d808043d4d
Arush Bartaria <arushbartaria@gmail.com>
2020-01-02 02:06:17 +0530

    screening by mission status, priority and distance

--- backends/dms/drone_controller/scheduler.py
+++ backends/dms/drone_controller/scheduler.py
@@ -1,26 +1,36 @@
 import pyodbc
 import time
 from os import system as sys
+from math import sqrt
 
 connection_SQL = pyodbc.connect() ####### Won't put it here. I'll add these credentials during deployment.
 cursor_SQL = connection_SQL.cursor()
 
 class DroneStock():
     
-    def __init__(self,lat,long,mission_type,priority,expected_mission_duration):
-        self.lat = lat
+    def __init__(self,latitude,longitude,mission_type,priority,expected_mission_duration,num_drones=1):
+        self.lat = latitude
+        self.long = longitude
+        self.mission_type = mission_type
+        self.priority = priority
+        self.expected_mission_duration = expected_mission_duration
+        self.num_drones = num_drones
         self.nearest_drones = []
-        self.availablity = {}
-      # complete this part
+
+    def location_dist(loc1,loc2):
+        return sqrt((loc1[0]-loc2[0])**2 + (loc1[0]-loc2[0])**2)
     
-    def search_nearest():
-       #Rename the drones in the query, I don't remember the names of the attributes in the table
-        cursor_SQL.execute(f"SELECT TOP 6 drones WHERE lon BETWEEN {self.location[1]+0.0005} AND {self.location[1] - 0.0005} AND lat BETWEEN {self.location[0] + 0.0005} AND {self.location[0] - 0.0005} ORDER BY (POW(lon-{self.location[1]},2) + POW(lon-{self.location[0]},2) " )
+    def search_nearest(self):
+        cursor_SQL.execute(f"SELECT TOP {self.num_drones} drones WHERE mission_status == '111111' AND {self.priority} > mission_criticality AND location.STX BETWEEN {self.long+0.0005} AND {self.long - 0.0005} AND location.STY BETWEEN {self.lat + 0.0005} AND {self.lat - 0.0005} ORDER BY (POW(location.STX-{self.long},2) + POW(location.STY-{self.lat},2);")
         row = cursor_SQL.fetchone()
         while row:
-            self.nearest_drone.append(row)
+            if self.checkSuitable(row):
+                self.nearest_drones.append(row)
             row.fetchone()
         return self.nearest_drones
-    
-    #This is the main logic. Check the other conditions like availability, priority, mission_status, etc before assigning a drone to the given mission.
-    #I'm sleeping now. GN.
+
+    def findSuitable(self):
+        if self.num_drones is not 1:
+            self.nearest_drones.sort(key=lambda x: location_distance(drone[2],(self.lat,self.long))  ### Not sure if long comes first in DB
+            return self.nearest_drones[:self.num_drones]
+        return self.nearest_drones
